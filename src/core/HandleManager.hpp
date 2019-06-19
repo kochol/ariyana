2bbfe8d3a4c731cd223e8bf256d8b974877c6a7e
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2019-06-20 00:19:28 +0800

    Add elementBuffer

---
+++ src/core/HandleManager.hpp
@@ -0,0 +1,16 @@
+#include <cstdint>
+
+namespace ari
+{
+    namespace core
+    {
+        struct HandleManager
+        {
+			uint32_t GetNewHandle();
+
+
+        };
+        
+    } // namespace core
+    
+} // namespace ari
