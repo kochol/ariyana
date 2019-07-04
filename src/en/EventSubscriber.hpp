f1bca549e180bd6b71f86ee38862212a09daecb7
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2019-07-04 10:33:15 +0800

    Create EventSubscriber

---
+++ src/en/EventSubscriber.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+namespace ari::en
+{
+	class World;
+	class Entity;
+	class Component;
+	class FrameData;
+
+	namespace Internal
+	{
+		class BaseEventSubscriber
+		{
+		public:
+
+			virtual ~BaseEventSubscriber() = default;
+		};
+		
+	} // Internal
+
+	/**
+	* Subclass this as EventSubscriber<EventType> and then call World::subscribe() in order to subscribe to events. Make sure
+	* to call World::unsubscribe() or World::unsubscribeAll() when your subscriber is deleted!
+	*/
+	template<typename T>
+	class EventSubscriber: public Internal::BaseEventSubscriber
+	{
+	public:
+
+		virtual ~EventSubscriber() = default;
+
+		/**
+		* Called when an event is emitted by the world.
+		*/
+		virtual void Receive(World* world, const T& event) = 0;
+
+	}; // EventSubscriber
+
+
+	namespace events
+	{
+		// Called when a new entity is created.
+		struct OnEntityCreated
+		{
+			Entity* entity;
+		};
+
+		// Called when an entity is about to be destroyed.
+		struct OnEntityDestroyed
+		{
+			Entity* entity;
+		};
+
+		// Called when a component is assigned (not necessarily created).
+		template <class T>
+		struct OnComponentAssigned
+		{
+			Entity* entity;
+			T* component;
+		};
+
+		// Called when a component is removed
+		template <class T>
+		struct OnComponentRemoved
+		{
+			Entity* entity;
+			T* component;
+		};
+
+		struct OnFrameData
+		{
+			FrameData* frame_data;
+		};
+
+	} // events
+
+} // ari::en
