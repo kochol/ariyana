c474c5fb1a1ccba10cc9cde69358e8a75c8fb0e7
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2019-07-14 20:26:42 +0800

    Add Buffer

---
+++ src/core/memory/Buffer.cpp
@@ -0,0 +1,172 @@
+#include "Buffer.hpp"
+#include "Memory.hpp"
+#include "core/Assertion.hpp"
+
+namespace ari::core
+{
+	//------------------------------------------------------------------------------
+	Buffer::Buffer(): size(0), capacity(0), data(nullptr)
+	{
+	}
+
+	//------------------------------------------------------------------------------
+	Buffer::Buffer(Buffer&& rhs): size(rhs.size), capacity(rhs.capacity), data(rhs.data)
+	{
+		rhs.size = 0;
+		rhs.capacity = 0;
+		rhs.data = nullptr;
+	}
+
+	//------------------------------------------------------------------------------
+	Buffer::~Buffer()
+	{
+		destroy();
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::operator=(Buffer&& rhs)
+	{
+		this->destroy();
+		this->size = rhs.size;
+		this->capacity = rhs.capacity;
+		this->data = rhs.data;
+		rhs.size = 0;
+		rhs.capacity = 0;
+		rhs.data = nullptr;
+	}
+
+	//------------------------------------------------------------------------------
+	int Buffer::Size() const
+	{
+		return size;
+	}
+
+	//------------------------------------------------------------------------------
+	bool Buffer::Empty() const
+	{
+		return size == 0;
+	}
+
+	//------------------------------------------------------------------------------
+	int Buffer::Capacity() const
+	{
+		return capacity;
+	}
+
+	//------------------------------------------------------------------------------
+	int Buffer::Spare() const
+	{
+		return capacity - size;
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::Reserve(int numBytes)
+	{
+		// need to grow?
+		if ((this->size + numBytes) > this->capacity) {
+			const int newCapacity = this->size + numBytes;
+			this->alloc(newCapacity);
+		}
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::Add(const uint8_t* data, int numBytes)
+	{
+		this->Reserve(numBytes);
+		this->copy(data, numBytes);
+	}
+
+	//------------------------------------------------------------------------------
+	uint8_t* Buffer::Add(int numBytes)
+	{
+		this->Reserve(numBytes);
+		uint8_t* ptr = this->data + this->size;
+		this->size += numBytes;
+		return ptr;
+	}
+
+	//------------------------------------------------------------------------------
+	int Buffer::Remove(int offset, int numBytes)
+	{
+		a_assert_dbg(offset >= 0);
+		a_assert_dbg(numBytes >= 0);
+		if (offset >= this->size) {
+			return 0;
+		}
+		if ((offset + numBytes) >= this->size) {
+			numBytes = this->size - offset;
+		}
+		a_assert_dbg((offset + numBytes) <= this->size);
+		a_assert_dbg(numBytes >= 0);
+		if (numBytes > 0) {
+			int bytesToMove = this->size - (offset + numBytes);
+			if (bytesToMove > 0) {
+				Memory::Move(this->data + offset + numBytes, this->data + offset, bytesToMove);
+			}
+			this->size -= numBytes;
+			a_assert_dbg(this->size >= 0);
+		}
+		return numBytes;
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::Clear()
+	{
+		size = 0;
+	}
+
+	//------------------------------------------------------------------------------
+	const uint8_t* Buffer::Data() const
+	{
+		a_assert(data);
+		return data;
+	}
+
+	//------------------------------------------------------------------------------
+	uint8_t* Buffer::Data()
+	{
+		a_assert(data);
+		return data;
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::alloc(int newCapacity)
+	{
+		a_assert_dbg(newCapacity > this->capacity);
+		a_assert_dbg(newCapacity > this->size);
+
+		uint8_t* newBuf = (uint8_t*)Memory::Alloc(newCapacity);
+		if (this->size > 0) {
+			a_assert_dbg(this->data);
+			Memory::Copy(this->data, newBuf, this->size);
+		}
+		if (this->data) {
+			Memory::Free(this->data);
+		}
+		this->data = newBuf;
+		this->capacity = newCapacity;
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::destroy()
+	{
+		if (this->data) {
+			Memory::Free(this->data);
+		}
+		this->data = nullptr;
+		this->size = 0;
+		this->capacity = 0;
+	}
+
+	//------------------------------------------------------------------------------
+	void Buffer::copy(const uint8_t* ptr, int numBytes)
+	{
+		// NOTE: it is valid to call copy with numBytes==0
+		a_assert_dbg(this->data);
+		a_assert_dbg((this->size + numBytes) <= this->capacity);
+		Memory::Copy(ptr, this->data + this->size, numBytes);
+		this->size += numBytes;
+	}
+
+
+} // namespace ari::core
---
+++ src/core/memory/Buffer.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <cstdint>
+
+namespace ari::core
+{
+    class Buffer
+    {
+    public:
+        /// default constructor
+        Buffer();
+        /// move constructor
+        Buffer(Buffer&& rhs);
+        /// destructor
+        ~Buffer();
+
+        /// always force move-construct
+        Buffer(const Buffer& rhs) = delete;
+        /// always force move-assign
+        void operator=(const Buffer& rhs) = delete;
+
+        /// move-assignment
+        void operator=(Buffer&& rhs);
+
+        /// get number of bytes in buffer
+        int Size() const;
+        /// return true if empty
+        bool Empty() const;
+        /// get capacity in bytes of buffer
+        int Capacity() const;
+        /// get number of free bytes at back
+        int Spare() const;
+
+        /// make room for N more bytes
+        void Reserve(int numBytes);
+        /// add bytes to buffer
+        void Add(const uint8_t* data, int numBytes);
+        /// add uninitialized bytes to buffer, return pointer to start
+        uint8_t* Add(int numBytes);
+        /// remove a chunk of data from the buffer, return number of bytes removed
+        int Remove(int offset, int numBytes);
+        /// clear the buffer (deletes content, keeps capacity)
+        void Clear();
+        /// get read-only pointer to content (throws assert if would return nullptr)
+        const uint8_t* Data() const;
+        /// get read/write pointer to content (throws assert if would return nullptr)
+        uint8_t* Data();
+
+    private:
+        /// (re-)allocate buffer
+        void alloc(int newCapacity);
+        /// destroy buffer
+        void destroy();
+        /// append-copy content into currently allocated buffer, bump size
+        void copy(const uint8_t* ptr, int numBytes);
+
+        int size;
+        int capacity;
+        uint8_t* data;
+
+    };
+    
+} // namespace ari::core
