22b4158f69d8d04c936fa04f1cbab7df1259191a
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2019-07-25 22:52:20 +0800

    Add texCube test

---
+++ tests/05-texcube/texcube.cpp
@@ -0,0 +1,65 @@
+#include "gfx/gfx.hpp"
+#include "gfx/Application.hpp"
+#include "en/World.hpp"
+#include "3d/RenderSystem.hpp"
+#include "3d/SceneSystem.hpp"
+#include "3d/Camera.hpp"
+#include "3d/BoxShape.hpp"
+#include "fs_local/FileSystemLocal.hpp"
+#include "io/FileSystem.hpp"
+
+class TexCubeApp : public ari::Application
+{
+public:
+
+	~TexCubeApp() = default;
+
+	ari::gfx::GfxSetup* GetGfxSetup() override
+	{
+		return &m_setup;
+	}
+
+	void OnInit() override
+	{
+		// Add systems
+		m_world.AddSystem(&m_renderer);
+		m_world.AddSystem(&m_scene_mgr);
+		ari::io::RegisterFileSystem("file", &m_file_system_local);		
+
+		// Create entity and add box and camera
+		ari::en::EntityHandle entity = m_world.CreateEntity();
+		auto camera = m_world.CreateComponent<ari::en::Camera, ari::en::Node3D>();
+		camera.Component->Position.x = 3.f;
+		camera.Component->Position.y = 3.f;
+		camera.Component->Position.z = 3.f;
+		camera.Component->Target.z = 0.0f;
+		m_world.AddDerivedComponent<ari::en::Camera, ari::en::Node3D>(entity, camera);
+		auto box = m_world.CreateComponent<ari::en::BoxShape, ari::en::Node3D>();
+		box.Component->Texture = ari::gfx::CreateTexture("res:baboon.png");
+		m_pBox = box.Component;
+		m_world.AddDerivedComponent<ari::en::BoxShape, ari::en::Node3D>(entity, box);
+	}
+
+	void OnFrame() override
+	{
+		m_pBox->Rotation.x += 0.0001f;
+		m_pBox->Rotation.y += 0.0001f;
+		m_world.Update(0.01f);
+	}
+
+	void OnCleanup() override
+	{
+		
+	}
+
+private:
+
+	ari::gfx::GfxSetup			m_setup;
+	ari::en::World				m_world;
+	ari::en::RenderSystem		m_renderer;
+	ari::en::SceneSystem		m_scene_mgr;
+	ari::io::FileSystemLocal	m_file_system_local;
+	ari::en::BoxShape		*	m_pBox = nullptr;
+};
+
+ARI_MAIN(TexCubeApp)
