2e5e742b2e708be74b9508ce3b330737ae9e0728
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2020-05-07 16:17:27 +0800

    Update sprite.glsl

--- src/2d/Shaders/sprite.glsl
+++ src/2d/Shaders/sprite.glsl
@@ -9,15 +9,23 @@ uniform vs_params {
 };
 
 in vec4 pos;
+in vec2 uv0;
+out vec2 uv;
+
 void main() {
     gl_Position = mvp * pos;
+    uv = uv0;
 }
 @end
 
 @fs fs
 out vec4 frag_color;
+in vec2 uv;
+
+uniform sampler2D tex;
+
 void main() {
-    frag_color = vec4(1.0);
+    frag_color = texture(tex, uv);
 }
 @end
 
