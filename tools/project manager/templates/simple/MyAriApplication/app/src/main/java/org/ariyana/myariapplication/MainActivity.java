3c35af171b52e27c50fd01c7a5e50a40003abe00
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2020-08-24 11:51:46 +0800

    the new android project template

---
+++ tools/project manager/templates/simple/MyAriApplication/app/src/main/java/org/ariyana/myariapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package org.ariyana.myariapplication;
+
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class MainActivity extends NativeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
