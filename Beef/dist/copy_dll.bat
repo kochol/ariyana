d3beddc92c5c5bc2ef8893643dad3c5f318e3907
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-01-13 01:50:56 +0800

    add core and test it

--- Beef/dist/copy_dll.bat
+++ Beef/dist/copy_dll.bat
@@ -1,7 +1,8 @@
-copy ..\..\..\fips-build\ariyana\win64-vstudio-debug\src\c99\Debug\cari.lib .\win64\Debug\
+FOR /R "..\..\..\fips-build\ariyana\win64-vstudio-debug\src\" %%i IN (*.lib) DO COPY "%%i" ".\win64\Debug\"
 copy ..\..\..\fips-deploy\ariyana\win64-vstudio-debug\cari.dll .\win64\Debug\
 
-copy ..\..\..\fips-build\ariyana\server-win64-vstudio-debug\src\c99\Debug\cari.lib .\win64\ServerDebug\
+FOR /R "..\..\..\fips-build\ariyana\server-win64-vstudio-debug\src\" %%i IN (*.lib) DO COPY "%%i" ".\win64\ServerDebug\"
 copy ..\..\..\fips-deploy\ariyana\server-win64-vstudio-debug\cari.dll .\win64\ServerDebug\
 
+FOR /R "..\..\..\fips-build\ariyana\server-linux-ninja-debug\src\" %%i IN (*.a) DO COPY "%%i" ".\linux64\ServerDebug\"
 copy ..\..\..\fips-deploy\ariyana\server-linux-ninja-debug\libcari.so .\linux64\ServerDebug\
