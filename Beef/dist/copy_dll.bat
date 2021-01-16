d86f98dc88139658a19dac0cfcba07feb97adec5
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-01-16 10:54:02 +0800

    fix for release compile

--- Beef/dist/copy_dll.bat
+++ Beef/dist/copy_dll.bat
@@ -1,5 +1,7 @@
 FOR /R "..\..\..\fips-build\ariyana\cari-win64-vstudio-debug\" %%i IN (*.lib) DO COPY "%%i" ".\win64\Debug\"
 
+FOR /R "..\..\..\fips-build\ariyana\cari-win64-vstudio-release\" %%i IN (*.lib) DO COPY "%%i" ".\win64\Release\"
+
 FOR /R "..\..\..\fips-build\ariyana\server-win64-vstudio-debug\" %%i IN (*.lib) DO COPY "%%i" ".\win64\ServerDebug\"
 
 FOR /R "..\..\..\fips-build\ariyana\server-linux-ninja-debug\" %%i IN (*.a) DO COPY "%%i" ".\linux64\ServerDebug\"
