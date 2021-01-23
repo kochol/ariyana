9ce39cf432c97ee14ffcf7d693baa696b75cd199
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-01-23 12:24:03 +0800

    Update Canvas.cpp

--- src/2d/Canvas.cpp
+++ src/2d/Canvas.cpp
@@ -20,22 +20,15 @@ namespace ari::en
 		float sy = float(r.height) / float(gfx::g_setup.window.Height);
 		float s = sx_min(sx, sy);
 		_RealRect = this->Rect * s;
-		if (sy < sx)
-		{
-			_RealRect.x += sx_abs(r.width - gfx::g_setup.window.Width * s) / 2;
-		}
-		else if (sy > sx)
-		{
-			_RealRect.y += sx_abs(r.height - gfx::g_setup.window.Height * s) / 2;
-		}
 		gfx::SetViewportSize(_RealRect);
-		_finalMat[0] = sx_mat4_SRT(s, s, 1, 0, 0, 0, 
-			_RealRect.width / -2, _RealRect.height / -2, 0);
+		_finalMat[0] = sx_mat4_SRT(s, s, 1, 0, 0, 0, 0, 0, 0);
 	}
 
 	void Canvas::UpdateCamera(Camera2D* _camera, int _frameTurnIndex)
 	{
 		_camera->_finalMat[_frameTurnIndex] = _camera->_finalMat[_frameTurnIndex] * _finalMat[0];
+		_camera->_finalMat[_frameTurnIndex].m14 *= _finalMat[0].m11;
+		_camera->_finalMat[_frameTurnIndex].m24 *= _finalMat[0].m11;
 	}
 
 } // namespace ari::en
