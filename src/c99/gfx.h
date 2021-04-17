ee68eda0261dc36746094e2cb9bab6883cc81e7e
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-04-17 10:12:50 +0800

    clear some code

--- src/c99/gfx.h
+++ src/c99/gfx.h
@@ -4,33 +4,6 @@
 #include "macros.h"
 #include "core.h"
 
-struct WindowSetup
-{
-    int Width;
-    int Height;
-    bool FullScreen;
-    bool HighDpi;
-    float DpiScale;
-};
-
-struct GfxSetup
-{
-    WindowSetup window;
-    int sample_count;                   /* MSAA sample count */
-    int swap_interval;                  /* the preferred swap interval (ignored on some platforms) */
-};
-
-// Globals
-CARI_API bool SetupGfx(GfxSetup& setup);
-CARI_API uint64_t* GetFrameNumberPointer();
-CARI_API void SetClearColor(Quat* _quat);
-CARI_API void BeginDefaultPass();
-CARI_API void EndPass();
-CARI_API void Commit();
-CARI_API void Present();
-CARI_API void SetWindowSize(int _width, int _height, bool _soft);
-CARI_API void GetViewportSize(RectI& _rect); 
-
 // Texture
 CARI_HANDLE(TextureHandle)
 CARI_API bool IsValidTexture(uint32_t& _handle);
